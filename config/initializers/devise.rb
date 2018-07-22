e69d83109bb3d19875b6a08c48f6ef38d266f192
Diego Balseiro <dbalseiro@stackbuilders.com>
2018-07-22 00:50:57 -0500

    Fix travis tests

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,4 @@
 Devise.setup do |config|
   config.navigational_formats = [:json]
+  config.secret_key = 'd0d78c830f6259107bcc7ee7006f824d42823ad626e82096e753436c42ee3117933bc7c21d3949830362a30e648c7e581d34ba38abad0146d39601880bc96f75'
 end
